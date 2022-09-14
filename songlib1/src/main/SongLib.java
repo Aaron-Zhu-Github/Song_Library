9e53e224ccd9e4bd430b0734ae29ef81c7651754
Junren(Aaron) Zhu <107200188+Aaron-Zhu-Github@users.noreply.github.com>
2022-09-14 01:01:57 -0400

    Add files via upload

---
+++ songlib1/src/main/SongLib.java
@@ -0,0 +1,11 @@
+
+package main;
+
+import javafx.application.Application;
+import ui.App;
+
+public class SongLib {
+    public static void main(String[] args) {
+        Application.launch(App.class);
+    }
+}
